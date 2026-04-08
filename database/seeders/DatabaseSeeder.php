1bde287f19d4c1938cbe31be8425dc72aeb802ac
Shoaib Tariq <43224905+tshoaib6@users.noreply.github.com>
2026-04-09 04:25:39 +0500

    Seed default admin user and workspace

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Workspace;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(UserSeeder::class);
+        $user = User::updateOrCreate(
+            ['email' => 'admin@eraxon.com'],
+            [
+                'name' => 'Admin',
+                'email_verified_at' => now(),
+                'password' => Hash::make('qwerty'),
+                'locale' => 'en',
+            ]
+        );
+
+        $workspace = Workspace::firstOrCreate(
+            ['owner_id' => $user->id],
+            ['name' => 'Default Workspace']
+        );
+
+        $workspace->users()->syncWithoutDetaching([
+            $user->id => ['role' => Workspace::ROLE_OWNER],
+        ]);
+
+        if ($user->current_workspace_id !== $workspace->id) {
+            $user->forceFill(['current_workspace_id' => $workspace->id])->save();
+        }
     }
 }
